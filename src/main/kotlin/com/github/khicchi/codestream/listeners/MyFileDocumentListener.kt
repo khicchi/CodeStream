d3203e6b4f6be3dfb34a6365d79e64f3903bbd80
kicchi <hkececi@applausemail.com>
2024-08-10 22:55:32 +0300

    able to send file name and its content

--- src/main/kotlin/com/github/khicchi/codestream/listeners/MyFileDocumentListener.kt
+++ src/main/kotlin/com/github/khicchi/codestream/listeners/MyFileDocumentListener.kt
@@ -9,6 +9,7 @@ import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.RequestBody
+import org.json.JSONObject
 import java.io.IOException
 
 class MyFileDocumentListener : FileDocumentManagerListener {
@@ -16,15 +17,20 @@ class MyFileDocumentListener : FileDocumentManagerListener {
         val project = ProjectManager.getInstance().openProjects[0]
         val virtualFile: VirtualFile? = FileDocumentManager.getInstance().getFile(document)
         if (virtualFile != null && !virtualFile.isDirectory) {
+            val fileName = virtualFile.name
             val fileContent = document.text
-            sendCodeToWebsite(fileContent)
+            sendCodeToWebsite(fileName, fileContent)
         }
     }
 
-    private fun sendCodeToWebsite(code: String) {
+    private fun sendCodeToWebsite(fileName: String, code: String) {
         val client = OkHttpClient()
-        val mediaType = "text/plain".toMediaTypeOrNull()
-        val requestBody = RequestBody.create(mediaType, code)
+        val mediaType = "application/json; charset=utf-8".toMediaTypeOrNull()
+        val json = JSONObject().apply {
+            put("file", fileName)
+            put("code", code)
+        }
+        val requestBody = RequestBody.create(mediaType, json.toString())
         val request = Request.Builder()
             .url("http://localhost:8080/")
             .post(requestBody)
@@ -41,5 +47,4 @@ class MyFileDocumentListener : FileDocumentManagerListener {
             e.printStackTrace()
         }
     }
-
 }
